334e0ab06a3b06b9559c9e8fbbc2220282abdadc
P3 <ortizadrian628@gmail.com>
2018-11-06 11:10:20 -0800

    hey

--- webapp.py
+++ webapp.py
@@ -1,10 +1,24 @@
+
 from flask import Flask, request, Markup, render_template, flash, Markup
 import os
 import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def render_home():
-return render_template('index.html')
+    with open('county-demographics2.json') as demographics_data:
+        counties = json.load(demographics_data)
+    return render_template("home.html",candy = getStateOptions(counties))
+def getStateOptions(counties):
+    myList=[] 
+    for county in counties:
+        if not county("State") in myList:
+            myList
+    otptions = ""
+    for state in listOfStates:
+        options=options + Markup("<option value=\"" + s + "\">" + s + "</option>")
+    return options
+    
 if __name__=="__main__":
     app.run(debug=True, port=54321)
