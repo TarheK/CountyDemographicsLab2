3a53ae745dd2c6fb6801d3f72fac54a7ce9ff31f
P3 <ortizadrian628@gmail.com>
2018-11-07 12:38:18 -0800

    made ift run

--- webapp.py
+++ webapp.py
@@ -7,18 +7,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_home():
-    with open('county-demographics2.json') as demographics_data:
+    with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
-    return render_template("home.html",candy = getStateOptions(counties))
+    if "state" in request.args:
+        state = request.args ["state"]
+        return render_template("home.html",candy = getStateOptions(counties), funFactoid = funFactoid(state))
+    else:
+        return render_template("home.html",candy = getStateOptions(counties))
 def getStateOptions(counties):
     myList=[] 
     for county in counties:
-        if not county("State") in myList:
-            myList
-    otptions = ""
-    for state in listOfStates:
-        options=options + Markup("<option value=\"" + s + "\">" + s + "</option>")
+        if not county["State"] in myList:
+            myList.append (county["State"])
+    options = ""
+    for state in myList:
+        options=options + Markup("<option value=\"" + state + "\">" + state + "</option>")
     return options
+def funFactoid(state):
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    first = counties[0]["Education"]["Bachelor's Degree or Higher"]
+    for county in counties:
+        if county ["Education"]["Bachelor's Degree or Higher"]> first and state == county["State"]:    
+            first=county["Education"]["Bachelor's Degree or Higher"] 
+    return first
     
 if __name__=="__main__":
-    app.run(debug=True, port=54321)
+    app.run(debug=True)
